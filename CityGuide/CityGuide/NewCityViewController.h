be3e35565dd937e0947e5b79de253cffcf889c13
Rodrigo Fernandes Bulgarelli <eld_rbulgarelli@uolinc.com>
2016-02-15 17:58:56 -0200

    Ajustes

---
+++ CityGuide/CityGuide/NewCityViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewCityViewController.h
+//  CityGuide
+//
+//  Created by Rodrigo Fernandes Bulgarelli on 2/15/16.
+//  Copyright © 2016 Rodrigo Fernandes Bulgarelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewCityViewController : UIViewController
+
+@end
